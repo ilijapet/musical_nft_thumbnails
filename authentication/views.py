5f087f98299ce27c537678658e0a98572f517384
ilija <ilijapet@gmail.com>
2023-10-03 14:18:58 +0200

    authentication: login and logout

--- authentication/views.py
+++ authentication/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+# from django.contrib.auth.models import User
 
 
 
 class HomeView(TemplateView):
     template_name="home.html"
+    # template_name="navbar.html"
        
     def post(self, request):
         # check to see if loggin in
         if request.method == "POST":
             user_name = request.POST["username"]
             password = request.POST["password"]
-            print(user_name, password)
             user = authenticate(request, username=user_name, password=password)
             if user is not None:
                 login(request, user)
