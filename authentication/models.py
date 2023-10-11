cc47896f54a68c4fe1ad60dda6b9fdfb6368132f
ilija <ilijapet@gmail.com>
2023-10-11 13:05:47 +0200

    user profile page

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,40 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+
+
+class Customer(models.Model):
+    
+    CRYPTO = "CRYPTO"
+    CREDIT = "CREDIT"
+
+    OPTIONS = [
+        (CRYPTO, "cypto buyer"),
+        (CREDIT, "credit card buyer")
+    ]
+    
+    created_at = models.DateTimeField(auto_now=True)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name  = models.CharField(max_length=50, blank=True)
+    username = models.CharField(max_length=50, blank=True) 
+    email = models.EmailField(max_length=250, blank=True)
+    type = models.CharField( 
+        max_length = 20, 
+        choices = OPTIONS, 
+        default = 'CRYPTO'
+        ) 
+    total_no_of_nfts = models.IntegerField(default=0)
+    nft_ids = ArrayField(models.IntegerField(null=True, blank=True), null=True, blank=True) 
+    nft_metadata_ids = ArrayField(models.IntegerField(null=True, blank=True), null=True, blank=True) 
+
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name}")
+
+    
+
+# class NFTMetadata(models.Model):
+    # attributes = 
+    # descrition = text
+    # external_link = url
+    # img = link to ipfs
+    # name = name
--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Customer
 
 
 
@@ -10,8 +11,7 @@ class HomeView(TemplateView):
     template_name="home.html"
 
     def post(self, request):
-        # check to see if loggin in
-        if request.method == "POST":
+            # check to see if loggin in
             user_name = request.POST["username"]
             password = request.POST["password"]
             user = authenticate(request, username=user_name, password=password)
@@ -22,11 +22,13 @@ class HomeView(TemplateView):
             else:
                 messages.success(request, "There was An Error login in, please try again")
                 return redirect("home")  
-        else:
-            return render(request, "home.html", {})
         
-
-
+    def get(self, request):
+        if str(request.user) == "AnonymousUser":
+            return render(request, "home.html", {})
+        name = str(request.user)
+        customer = Customer.objects.get(username=name)
+        return render(request, "home.html", {"customer": customer})
 
 class LogoutUser(TemplateView):    
     def get(self, request):
@@ -43,11 +45,13 @@ class RegisterUser(TemplateView):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
+
             # Authenticate use and login
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             login(request, user)
+            Customer.objects.create(username=user, email=user.email, first_name=user.first_name, last_name=user.last_name)
             messages.success(request,  "You have been sucesfuly logged in ")
             return redirect("home")
         return render(request, "register.html", {"form": form}) 
